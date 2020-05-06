2a29192ccc49b17c4f2196c7bde0f307d9720e31
nicolago <vodafonenicola@yahoo.it>
2020-05-06 20:52:59 +0200

    back to cross entropy batch 64

--- Main.py
+++ Main.py
@@ -43,13 +43,13 @@ path_to_csv_trainval = [root_path+"egtea/training1.csv", root_path+"egtea/valida
 ### SOME MODEL'S VARIABLES ###
 
 input_dim = [1024, 1024, 352]
-batch_size = 4
+batch_size = 32
 seq_len = 14
 
 learning_rate = 0.001
 
 
-epochs = 100
+epochs = 10
 
 display_every = 10
 
