7c5a610598d200fe40c9aa168d9addcb774224d3
nicolago <vodafonenicola@yahoo.it>
2020-05-12 11:18:57 +0200

    try only with obj

--- Model.py
+++ Model.py
@@ -9,7 +9,7 @@ class BaselineModel(torch.nn.Module):
     def __init__(self, batch_size, seq_len, input_size, dropout=0.8, num_classes=106):
         super(BaselineModel, self).__init__()
 
-        self.branches = torch.nn.LSTM(input_size[0], 1024, 3, batch_first=True)
+        self.branches = torch.nn.LSTM(input_size[2], 1024, 3, batch_first=True)
         """
         self.branches = nn.ModuleDict({
             "rgb": torch.nn.LSTM(input_size[0], 1024, seq_len),  # input of lstm is 1024 (vector of input), hidden units are 1024, num layers is 14 (6 enc + 8 dec)
