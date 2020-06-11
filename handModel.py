9ae758e56ceb40c217421b9d545e672c9c53ef76
nicolago <vodafonenicola@yahoo.it>
2020-06-11 17:15:29 +0200

    without augmentation

--- handModel.py
+++ handModel.py
@@ -171,8 +171,8 @@ def to_tensor(x, **kwargs):
         #x = np.expand_dims(x, axis=2)
         x = np.stack((x,) * 3, axis=-1)
         x = x/255
-        #print(x.shape)
-        #print(x)
+        print(x.shape)
+        print(x)
 
 
     npad = ((8, 8), (0, 0), (0, 0))
