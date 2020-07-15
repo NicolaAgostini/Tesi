53e99837eb8214d954aedb9e1ee000f4e4465789
Nicola <vodafonenicola@yahoo.it>
2020-07-15 12:08:23 +0200

    try 3 br correct obj vn 0.2

--- New_Features_obj.py
+++ New_Features_obj.py
@@ -8,12 +8,13 @@ path_of_dir = "/home/2/2014/nagostin/Desktop/"
 
 for file in os.listdir(path_of_dir+"newfeat"):
     print("Processing "+ file)
-    env = lmdb.open(path_of_dir+'hand_obj_newfeat', map_size=1099511627776)
+    env = lmdb.open(path_of_dir+'hand_obj_newfeat', map_size=1099511627776, lock=False)
     video_name = file.split("_")[0] + '_frame_{:010d}.jpg'
     detections = np.load(path_of_dir+"newfeat/"+file, allow_pickle=True, encoding='bytes')
 
     for i, dets in enumerate(tqdm(detections,'Extracting features')):
         feat = dets
+        print(dets.shape)
         key = video_name.format(i+1)
         with env.begin(write=True) as txn:
             txn.put(key.encode(),feat)
