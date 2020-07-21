627863b348ff56d37d36e11d1ac0d76f09a18f0e
nicolago <vodafonenicola@yahoo.it>
2020-07-21 16:35:09 +0200

    new feat ls vn 0.2

--- Main.py
+++ Main.py
@@ -30,9 +30,9 @@ print("DEVICE= "+device)
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd"]
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd"]
 #path_to_lmdb = [root_path + "obj_correct"]
-path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd",root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd",root_path + "hand_obj_newfeat"]
+#path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd",root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd",root_path + "hand_obj_newfeat"]
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd",root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd",root_path + "obj_min"]
-#path_to_lmdb = [root_path + "hand_obj_newfeat"]
+path_to_lmdb = [root_path + "hand_obj_newfeat"]
 
 ### PATH OF TXT FOR TRAINING AND VALIDATION ###
 
@@ -65,7 +65,7 @@ seq_len = 14
 learning_rate = 0.00001
 
 
-epochs = 25
+epochs = 30
 
 display_every = 10
 
