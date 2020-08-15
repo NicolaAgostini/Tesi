69a41054835df90a5436c4894a26c035fedf672b
Nicola <vodafonenicola@yahoo.it>
2020-08-15 08:56:27 +0200

    fix features extraction

--- Features_obj.py
+++ Features_obj.py
@@ -5,9 +5,10 @@ import os
 
 
 path_of_dir = "/home/2/2014/nagostin/Desktop/"
+"""
 dict = {}
 count=0
-"""
+
 for file in os.listdir(path_of_dir+"video"):
     print("Processing "+ file)
     env = lmdb.open(path_of_dir+'obj_min', map_size=1099511627776)
@@ -30,13 +31,13 @@ for file in os.listdir(path_of_dir+"video"):
 #standard feature creation of array of 352 for each frame
 for file in os.listdir(path_of_dir+"video"):
     print("Processing "+ file)
-    env = lmdb.open(path_of_dir+'obj_min_FT', map_size=1099511627776)
+    env = lmdb.open(path_of_dir+'obj_54_FT', map_size=1099511627776)
     video_name = file.split("_")[0] + '_frame_{:010d}.jpg'
     detections = np.load(path_of_dir+"video/"+file, allow_pickle=True, encoding='bytes')
 
     for i, dets in enumerate(tqdm(detections,'Extracting features')):
         feat = np.zeros(54, dtype='float32')
-        print(dets)
+        #print(dets)
         for d in dets:
             feat[int(d[0])]+=d[5]
         key = video_name.format(i+1)
