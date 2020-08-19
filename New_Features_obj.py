a213fd3a17e8138649eab7336913bbdfc17c9ddd
nicolago <vodafonenicola@yahoo.it>
2020-08-19 09:55:14 +0200

    mdb new feat

--- New_Features_obj.py
+++ New_Features_obj.py
@@ -11,7 +11,7 @@ for file in os.listdir(path_of_dir+"newfeat"):
     env = lmdb.open(path_of_dir+'hand_obj_newfeat', map_size=1099511627776)
     video_name = file.split("_")[0] + '_frame_{:010d}.jpg'
     detections = np.load(path_of_dir+"newfeat/"+file, allow_pickle=True, encoding='bytes')
-
+    n_feat = 0
     for i, dets in enumerate(tqdm(detections,'Extracting features')):
         feat = np.zeros(54, dtype='float32')
         for index,value in enumerate(dets):
@@ -20,9 +20,11 @@ for file in os.listdir(path_of_dir+"newfeat"):
         #print(feat.shape)
         key = video_name.format(i+1)
         #print(key)
-        print(np.count_nonzero(feat))
+        n_feat = n_feat + np.count_nonzero(feat)
 
-        """
+
+
         with env.begin(write=True) as txn:
             txn.put(key.encode(),feat)
-        """
+
+    print("non vero feat = "+ str(n_feat))
