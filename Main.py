1e3fc9c9fdac4a92c5cb1e52a96f6fb08bf5bcbc
nicolago <vodafonenicola@yahoo.it>
2020-08-29 19:24:54 +0200

    try best on split 2

--- Main.py
+++ Main.py
@@ -22,7 +22,7 @@ print("DEVICE= "+device)
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd",root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd"]
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd", root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd", root_path + "obj"]  # the folders that contain the .mdb files
 
-path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd"]
+#path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd"]
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd"]
 #path_to_lmdb = [root_path + "obj"]
 #path_to_lmdb = [root_path + "obj_FT"]
@@ -31,13 +31,13 @@ path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcf
 #path_to_lmdb = [root_path + "hand_obj_newfeat"]
 #path_to_lmdb = [root_path + "obj_54_FT"]
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd",root_path + "obj_54_FT"]
-"""
-path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd",
-                root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd",
+
+path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s2_rgb_model_best_fcfull_hd",
+                root_path + "egtea/TSN-C_3_egtea_action_CE_s2_flow_model_best_fcfull_hd",
                 root_path + "obj_54_FT",
                 root_path + "hand_obj_newfeat"]
                 
-"""
+
 
 ### PATH OF TXT FOR TRAINING AND VALIDATION ###
 
@@ -53,7 +53,7 @@ groundtruth_path_test = [root_path+"egtea/action_annotation/test_split1.txt",
 ###
 
 
-path_to_csv_trainval = [root_path+"egtea/training1.csv", root_path+"egtea/validation1.csv"]  # path of train val csv
+path_to_csv_trainval = [root_path+"egtea/training2.csv", root_path+"egtea/validation2.csv"]  # path of train val csv
 
 path_to_csv_test = root_path+"egtea/test1.csv"  # for test dataloader
 
@@ -131,7 +131,7 @@ def main():
 
 
     #split_frames_objDect("/Volumes/Bella_li/frames")
-    path = initialize_trainval_csv(3)  # to generate training and validation csv depending on split defined by authors of egtea gaze +
+    #path = initialize_trainval_csv(3)  # to generate training and validation csv depending on split defined by authors of egtea gaze +
 
     #smoothed_labels = label_smoothing("glove")  # for smoothed labels
     #split_val_train()
@@ -143,6 +143,7 @@ def main():
                       SmoothedCrossEntropy(device=device, smooth_factor=0.6, smooth_prior="uniform",
                                            action_embeddings_csv_path="action_embeddings.csv", reduce_time="mean"),
                       SmoothedCrossEntropy(device=device, smooth_factor=0.6, smooth_prior="verb-noun", action_embeddings_csv_path="vn_prior.csv", reduce_time="mean")]
+
     for criterion in criterion_list:
         model = BaselineModel(batch_size, seq_len, input_dim)
 
@@ -171,6 +172,31 @@ def main():
 
 
     """
+
+    model = BaselineModel(batch_size, seq_len, input_dim)
+
+    model.to(device)
+    print(model)
+
+    # if mode == "train":
+    data_loader_train = get_dataset(path_to_csv_trainval[0], batch_size, 4)  # loader for training
+    data_loader_val = get_dataset(path_to_csv_trainval[1], batch_size, 4)  # loader for validation
+
+    # data_loader_train = get_mock_dataloader()
+    # data_loader_val = get_mock_dataloader()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # criterion = SmoothedCrossEntropy(device=device, smooth_factor=0.6, smooth_prior="verb-noun", action_embeddings_csv_path="vn_prior.csv", reduce_time="mean")
+    criterion = SmoothedCrossEntropy(device=device, smooth_factor=0.6, smooth_prior="glove", action_embeddings_csv_path="action_embeddings.csv", reduce_time="mean")
+    if mode == "train":
+        train_val(model, [data_loader_train, data_loader_val], optimizer, epochs, criterion)  # with smoothed labels
+
+    if mode == "test":
+        data_loader_test = get_dataset(path_to_csv_test, batch_size, 4)
+        epoch, perf, best_perf = load_model(model)  # load the best model saved
+        print("Testing model" + str(best_perf))
+        test_model(model, data_loader_test)
 
 
 def train_val(model, loaders, optimizer, epochs, criterion, resume = False):
