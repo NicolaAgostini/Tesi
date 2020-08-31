fc7c9274f1e387d862462ebbfc85ecff290f0471
nicolago <vodafonenicola@yahoo.it>
2020-08-31 10:27:19 +0200

    best nls

--- Main.py
+++ Main.py
@@ -19,7 +19,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("DEVICE= "+device)
 
 
-path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s3_rgb_model_best_fcfull_hd",root_path + "egtea/TSN-C_3_egtea_action_CE_s3_flow_model_best_fcfull_hd"]
+#path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd",root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd"]
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd", root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd", root_path + "obj"]  # the folders that contain the .mdb files
 
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd"]
@@ -31,13 +31,13 @@ path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s3_rgb_model_best_fcf
 #path_to_lmdb = [root_path + "hand_obj_newfeat"]
 #path_to_lmdb = [root_path + "obj_54_FT"]
 #path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd",root_path + "obj_54_FT"]
-"""
-path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s3_rgb_model_best_fcfull_hd",
-                root_path + "egtea/TSN-C_3_egtea_action_CE_s3_flow_model_best_fcfull_hd",
+
+path_to_lmdb = [root_path + "egtea/TSN-C_3_egtea_action_CE_s1_rgb_model_best_fcfull_hd",
+                root_path + "egtea/TSN-C_3_egtea_action_CE_s1_flow_model_best_fcfull_hd",
                 root_path + "obj_54_FT",
                 root_path + "hand_obj_newfeat"]
                 
-"""
+
 
 ### PATH OF TXT FOR TRAINING AND VALIDATION ###
 
@@ -53,7 +53,7 @@ groundtruth_path_test = [root_path+"egtea/action_annotation/test_split1.txt",
 ###
 
 
-path_to_csv_trainval = [root_path+"egtea/training3.csv", root_path+"egtea/validation3.csv"]  # path of train val csv
+path_to_csv_trainval = [root_path+"egtea/training1.csv", root_path+"egtea/validation1.csv"]  # path of train val csv
 
 path_to_csv_test = root_path+"egtea/test1.csv"  # for test dataloader
 
@@ -188,9 +188,9 @@ def main():
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     # criterion = SmoothedCrossEntropy(device=device, smooth_factor=0.6, smooth_prior="verb-noun", action_embeddings_csv_path="vn_prior.csv", reduce_time="mean")
-    criterion = SmoothedCrossEntropy(device=device, smooth_factor=0.6, smooth_prior="glove", action_embeddings_csv_path="action_embeddings.csv", reduce_time="mean")
+    #criterion = SmoothedCrossEntropy(device=device, smooth_factor=0.6, smooth_prior="glove", action_embeddings_csv_path="action_embeddings.csv", reduce_time="mean")
     if mode == "train":
-        train_val(model, [data_loader_train, data_loader_val], optimizer, epochs, criterion)  # with smoothed labels
+        train_val(model, [data_loader_train, data_loader_val], optimizer, epochs)  # with smoothed labels
 
     if mode == "test":
         data_loader_test = get_dataset(path_to_csv_test, batch_size, 4)
@@ -199,7 +199,7 @@ def main():
         test_model(model, data_loader_test)
 
 
-def train_val(model, loaders, optimizer, epochs, criterion, resume = False):
+def train_val(model, loaders, optimizer, epochs, resume = False):
     """
 
     :param model:
@@ -250,16 +250,16 @@ def train_val(model, loaders, optimizer, epochs, criterion, resume = False):
                     preds = preds.contiguous()
 
                     # linearize predictions
-                    #linear_preds = preds.view(-1, preds.shape[-1])  # (batch * 8 , 106)
+                    linear_preds = preds.view(-1, preds.shape[-1])  # (batch * 8 , 106)
 
-                    linear_labels = y.unsqueeze(1).expand(-1, preds.shape[1]).contiguous()  # for smoothed label
+                    #linear_labels = y.unsqueeze(1).expand(-1, preds.shape[1]).contiguous()  # for smoothed label
 
                     #print(linear_labels.size())
-                    #linear_labels = y.view(-1, 1).expand(-1, preds.shape[1]).contiguous().view(-1)
+                    linear_labels = y.view(-1, 1).expand(-1, preds.shape[1]).contiguous().view(-1)
 
-                    #loss = F.cross_entropy(linear_preds, linear_labels)
+                    loss = F.cross_entropy(linear_preds, linear_labels)
 
-                    loss = criterion(preds, linear_labels)  # for smoothed labels
+                    #loss = criterion(preds, linear_labels)  # for smoothed labels
 
                     #print(loss)
 
--- Model.py
+++ Model.py
@@ -11,7 +11,9 @@ class BaselineModel(torch.nn.Module):
         #self.lstm = torch.nn.LSTM(input_size[0], 1024, 1, batch_first=True)
 
         self.branches = torch.nn.ModuleList([torch.nn.LSTM(input_size[0], 1024, 1, batch_first=True),
-                                             torch.nn.LSTM(input_size[1], 1024, 1, batch_first=True)])
+                                             torch.nn.LSTM(input_size[1], 1024, 1, batch_first=True),
+                                             torch.nn.LSTM(input_size[2], 1024, 1, batch_first=True),
+                                             torch.nn.LSTM(input_size[3], 1024, 1, batch_first=True)])
 
 
         """
@@ -24,7 +26,7 @@ class BaselineModel(torch.nn.Module):
         self.seq_len = seq_len
         self.batch_size = batch_size
         self.dropout = torch.nn.Dropout(dropout)
-        self.fc = torch.nn.Linear(1024*2, num_classes)  # without seq_len because i want my output on every timestamp from 0 to 2s of observations
+        self.fc = torch.nn.Linear(1024*4, num_classes)  # without seq_len because i want my output on every timestamp from 0 to 2s of observations
 
         #self.fc = torch.nn.Linear(1024*3, num_classes)
         self.num_classes = num_classes
